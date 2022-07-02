7ec147566db1f50acd18626baeaf4b0745f8c911
Facundo <facundoarueda@hotmail.com>
2022-07-02 16:22:15 -0300

    Add Data Json

--- clonTestimonios/clon-testimonios/src/componentes/Testimonio.js
+++ clonTestimonios/clon-testimonios/src/componentes/Testimonio.js
@@ -13,7 +13,7 @@ function Testimonio(props) {
           <strong>{props.nombre}</strong> en {props.pais}
         </p>
         <p className='cargo-testimonio'>{props.cargo} en <strong>{props.empresa}</strong></p>
-        <p className='texto-testimonio'>"{props.testimonio}"</p>
+        <p className='texto-testimonio'>"{props.descripcion}"</p>
       </div>
     </div>
   );
